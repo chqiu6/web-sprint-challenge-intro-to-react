86ed4d8c27c2f4102a6ef6ce04c60609468a7a56
chqiu6 <chucky-qiu@lambdastudents.com>
2020-08-28 20:52:28 -0400

    currently working on styled components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const NameStyle = styled.h1`
+font-size: 2rem;
+background-color : pink;
+
+`
+
+const Character = (props) => {
+    return(
+        <div className = "starwars-characters">
+            <NameStyle>
+            <h1>{props.name}</h1>
+            </NameStyle>
+            <div className = "characters-info">
+                <p>{props.birthyear}</p>
+                <p>{props.gender}</p>
+                <p>{props.height}</p>
+                <p>{props.mass}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Character;
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Character from "./Character";
+
+export default function CharacterList() {
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios
+        .get(`https://swapi.dev/api/people/`)
+        //birth_year, eye_color, films, gender, hair_color, height, homeworld, mass, name, skin_color, created, edited, species, starships, url, vehicles
+        .then(res => {
+            console.log("Res data : ", res.data.results);
+            setCharacters(res.data.results);
+        })
+        .catch(err =>{ 
+            console.log("Err msg :", err);
+        });
+
+    },[]);
+
+    return(
+        <div className = "CharacterData">
+            {
+                characters.map((character, index) => {
+                    return (
+                        <Character 
+                        key = {index}
+                        name = {character.name}
+                        birthyear = {character.birth_year}
+                        gender = {character.gender}
+                        mass = {character.mass}
+                        height = {character.height}
+                        />
+                    );
+                })
+            }
+        </div>
+    )
+}
